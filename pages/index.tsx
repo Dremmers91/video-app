a0822b294fa55caf299e1cb6b91b818cd1008daa
Dayton Remmers <hello.dremmers+github@gmail.com>
2023-10-28 17:02:25 -0500

    Build Navigation bar for logged in user

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
+import Navbar from "@/components/Navbar";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import { type NextPageContext } from "next"
-import { getSession, signOut } from "next-auth/react"
+import { getSession } from "next-auth/react"
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -24,9 +25,7 @@ const { data: user } = useCurrentUser();
 
   return (
     <>
-      <h1 className="text-3xl text-green-500">Netflix Clone</h1>
-      <p className="text-white">Logged in as: {user?.name}</p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout!</button>
+      <Navbar />
     </>
   )
 }
