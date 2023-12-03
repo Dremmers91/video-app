5ac5d8a288bed618165ef682bf1771761ba54c45
Dayton Remmers <hello.dremmers+github@gmail.com>
2023-12-02 18:40:21 -0600

    Completed Info modal for movies - Still need to find fix for add to favorites issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Billboard from "@/components/Billboard";
+import InfoModal from "@/components/InfoModal";
 import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useFavorites from "@/hooks/useFavorites";
+import useInfoModal from "@/hooks/useInfoModal";
 import useMovieList from "@/hooks/useMovieList";
 import { type NextPageContext } from "next"
 import { getSession } from "next-auth/react"
@@ -28,9 +30,11 @@ export default function Home() {
   const { data: user } = useCurrentUser();
   const { data: movies = [] } = useMovieList();
   const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModal();
 
   return (
     <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
       <Billboard />
       <div className="pb-40">
