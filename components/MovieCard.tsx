a51dc90c9296c6ac161bf83cb52b6f573c58b52a
Dayton Remmers <hello.dremmers+github@gmail.com>
2023-12-09 01:12:07 -0600

    Remove Favotire button until issue is fixed

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -23,7 +23,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
                         <div className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300" onClick={() => router.push(`/watch/${data?.id}`)}>
                             <BsFillPlayFill size={30} />
                         </div>
-                        <FavoriteButton movieId={data?.id} />
+                        {/* <FavoriteButton movieId={data?.id} /> */}
                         <div onClick={() => openModal(data?.id)} className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
                             <BiChevronDown size={30} className="text-white group-hover/item:text-neutral-300 w-4" />
                         </div>
